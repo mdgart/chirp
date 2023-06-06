282498bbd1c5e2307d91063a7d6b2865908ac287
Mauro <mauro.degiorgi@gmail.com>
2023-06-06 15:18:53 -0600

    Add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from '@clerk/themes';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}  appearance={{
+
+      baseTheme: dark,
+      variables: {
+
+        colorPrimary: "white",
+
+        colorText: "white"
+
+      }
+
+    }}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
